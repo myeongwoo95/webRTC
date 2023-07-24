58e3cf61cfa4445f02b317a512b8b21c17a664b2
walle <myeongwoo95@naver.com>
2023-07-25 00:26:56 +0900

    :>

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -22,12 +22,18 @@ async function getCameras() {
     // 그 중 카메라들만 가져온다.
     const cameras = devices.filter((device) => device.kind === "videoinput");
 
+    // 현재 사용중인 카메라의 이름(label)을 저장
+    const currentCamera = myStream.getVideoTracks()[0];
+
     // 카메라 갯수만큼 option을 만들고 select에 추가한다.
     //  innerText에는 카메라의 장비의 이름을, value에는 카메라의 고유 ID를 넣는다.
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label == camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (error) {
@@ -35,23 +41,37 @@ async function getCameras() {
   }
 }
 
-// 최초에는 cameraDeviceId없이 로딩하고,
-async function getMedia(cameraDeviceId) {
-  const initialConstrains = {};
+// 파라미터 deviceId는 카메라 ID이다.
+// 이 함수는 최초에 파라미터 값 deviceId 없이 바로 실행되는 함수이다.
+async function getMedia(deviceId) {
+  // 미디어 스트림을 요청할 때 사용하는 초기 제약 조건으로
+  // 초기에 유저가 카메라를 선택하지 않은 상황에서 사용되고
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+
+  // 유저가 특정 카메라를 선택한다면 이것을 사용하는데 특정 카메라를 사용한다는 의미이다.
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: "myExactCameraOrBustDeviceId" } },
+  };
 
   try {
     // 스트림을 얻는다. (오디오와 비디오 트랙)
     // 브라우저가 사용자에게 카메라와 오디오의 접근 요구한다.
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
 
     // myFace는 HTML video인데 srcObject 속성에 스트림 객체 넣어줌으로써, video 태그와 스트림 객체를 연결한다.
     myFace.srcObject = myStream;
 
     // 사용자 카메라를 HTML에 select으로 그려주기
-    await getCameras();
+    // 최초의 한번만 그려주기 위해서 if문 처리
+    if (!cameraDeviceId) {
+      await getCameras();
+    }
   } catch (error) {
     console.log(error);
   }
